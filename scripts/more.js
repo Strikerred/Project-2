7102527509f473da11280104746c627cdd8e91e6
Lucas <woodsman.lucas@gmail.com>
2019-11-03 16:42:00 -0800

    Everything is broken tried reverting that didn't help

--- scripts/more.js
+++ scripts/more.js
@@ -1,16 +1,16 @@
+/* global $ */
+
 console.log($('.more-btn'))
-$('.courses').on('click', '.more-btn', display);
+$('.courses').on('click', '.more-btn', display)
 
-function display() {
-    if($(this).parent().parent().children('p').children('.more').css('display') == 'none'){
-        $(this).parent().parent().children('p').children('.more').css('display', 'inline');
-        $(this).parent().parent().children('p').children('.dots').css('display', 'none');
-        $(this).html('Read Less')
-
-    } else {
-        $(this).parent().parent().children('p').children('.more').css('display', 'none');
-        $(this).parent().parent().children('p').children('.dots').css('display', 'inline');
-        $(this).html('Read More')
-    }
-
+function display () {
+  if ($(this).parent().parent().children('p').children('.more').css('display') == 'none') {
+    $(this).parent().parent().children('p').children('.more').css('display', 'inline')
+    $(this).parent().parent().children('p').children('.dots').css('display', 'none')
+    $(this).html('Read Less')
+  } else {
+    $(this).parent().parent().children('p').children('.more').css('display', 'none')
+    $(this).parent().parent().children('p').children('.dots').css('display', 'inline')
+    $(this).html('Read More')
+  }
 };
